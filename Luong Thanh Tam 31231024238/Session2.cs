088f13c29963ca04b7379288a1568a4ba76ac3c4
Dell <Dell@THANHTAM>
2024-10-16 16:14:17 +0700

    Add project files.

---
+++ Luong Thanh Tam 31231024238/Session2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luong_Thanh_Tam_31231024238
+{
+    internal class Session2
+    {
+        private static void Main2()
+        {
+            //Ex1();
+            //Ex2();
+            Ex3();
+
+            Console.ReadKey();
+        }
+
+        private static void Ex1()
+        {
+            Console.WriteLine("Enter your Celius: ");
+            double Celius = double.Parse(Console.ReadLine());
+            double K = Celius + 273;
+            double F = Celius * 1.8 + 32;
+            Console.WriteLine($"Your Celius is {Celius}. Convert to K = {K} and F = {F}");
+
+            Console.ReadKey();
+        }
+        private static void Ex2()
+        {
+            Console.WriteLine("Enter your radius:");
+            double radius = double.Parse(Console.ReadLine());
+            double surface = 4 * Math.PI * Math.Sqrt(radius);
+            double volume = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+            Console.WriteLine($"Your radius = {radius} . Result: Surface = {surface} , Volume = {volume}");
+        }
+        private static void Ex3()
+        {
+            Console.WriteLine("Enter your first number: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second value: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} = {a / b}");
+            Console.WriteLine($"{a} mod {b} = {a % b}");
+        }
+    }
+}
