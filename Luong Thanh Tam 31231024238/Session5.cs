93f99ccae3eb747db80a284c8d5f681762f591ad
thanhtam915 <tamluong.31231024238@st.ueh.edu.vn>
2024-10-30 16:16:35 +0700

    Session 5

---
+++ Luong Thanh Tam 31231024238/Session5.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Luong_Thanh_Tam_31231024238
+{
+    internal class Session5
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("Nhap 3 chu so cach nhau bang khoang trang: ");
+            //string n1 = Console.ReadLine();
+            //string[] n1string = n1.Split(' ');
+            //int[] n1array = Array.ConvertAll(n1string, int.Parse);
+            //List<int> n1list = new List<int>(n1array);
+            //int max = FindMax(n1list);
+            //Console.WriteLine($"Gia tri max la {max}");
+
+
+            //int n;
+            //do
+            //{
+            //    Console.WriteLine("Nhap gia tri n:");
+            //    bool res = int.TryParse(Console.ReadLine(), out n);
+            //    if (!res)
+            //        Console.WriteLine("Nhap mot so nguyen, khong phai chuoi.");
+            //    else if (n < 0)
+            //        Console.WriteLine("Nhap mot so nguyen duong.");
+            //    else
+            //    {
+            //        break;
+            //    }
+            //} while (true);
+
+            //Console.WriteLine($"n = {n}, fractorial = {CaculateFactorial(n)}");
+
+
+
+            //Console.WriteLine("Nhap mot so duong: ");
+            //int number;
+            //bool res = int.TryParse(Console.ReadLine(), out number);
+
+            //if (res && number > 0)
+            //{
+            //    Console.WriteLine($"Cac so nguyen to nho hon {number} la:");
+            //    PrintPrimeNumberLessThan(number);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Vui long nhap mot so nguyen duong.");
+            //}
+
+
+            Console.WriteLine("Nhap mot so duong: ");
+            int number;
+            bool res = int.TryParse(Console.ReadLine(), out number);
+
+            if (res && number > 0)
+            {
+                Console.WriteLine($" {number} so nguyen to dau tien la:");
+                PrintNFirstPrimeNumbers(number);
+            }
+            else
+            {
+                Console.WriteLine("Vui long nhap mot so nguyen duong.");
+            }
+
+
+            Console.ReadKey();
+        }
+        public static int FindMax(List<int> numbers)
+        {
+            if (numbers == null || numbers.Count == 0)
+                throw new ArgumentException("Danh sách không được rỗng và phải có ít nhất một phần tử.");
+
+            int max = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                    max = num;
+            }
+
+            return max;
+        }
+        public static int CaculateFactorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public static bool IsPrime (int number3)
+        {
+            if (number3 <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number3); i++)
+            {
+                if (number3 % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        public static void PrintPrimeNumberLessThan(int number4a)
+        {
+            for (int i = 2; i < number4a; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void PrintNFirstPrimeNumbers(int n4b)
+        {
+            int count = 0;
+            int num = 2;
+
+            while (count < n4b)
+            {
+                if (IsPrime(num))
+                {
+                    Console.Write(num + " ");
+                    count++;
+                }
+                num++;
+            }
+            Console.WriteLine();
+        }
+        public static void Ex5(int number5)
+        public static void Ex6(int number6)
+        {
+
+        }
+    }
+}
---
+++ Luong Thanh Tam 31231024238/TaiXiu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luong_Thanh_Tam_31231024238
+{
+    internal class TaiXiu
+    {
+        public static void Main()
+        {
+            gameengine();
+        }
+
+        public static int RollDice()
+        {
+            Random rnd = new Random();
+            int die1 = rnd.Next(6) + 1;
+            int die2 = rnd.Next(6) + 1;
+            int die3 = rnd.Next(6) + 1;
+            int sumofDie = die1 + die2 + die3;
+            return sumofDie;
+        }
+
+        public static void playOneRound()
+        {
+            int comDice = RollDice();
+            Console.Write("Ban doan tai hay xiu <T/X>: ");
+            string userguessing = Console.ReadLine();
+            Console.WriteLine($"KQ cua may la {comDice}");
+            if (userguessing.ToUpper().Equals("T"))
+            {
+                if (comDice >= 10)
+                    Console.WriteLine("You win!");
+                else
+                    Console.WriteLine("You lose!");
+            }
+            else if (userguessing.ToUpper().Equals("X"))
+            {
+                if (comDice < 10)
+                    Console.WriteLine("You win!");
+                else
+                    Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("Vui long nhap dung!");
+            }
+        }
+
+        public static void gameengine()
+        {
+            do
+            {
+                playOneRound();
+                Console.WriteLine("Ban choi tiep khong <C/K>? ");
+                string choice = Console.ReadLine();
+                if (choice.ToUpper().Equals("K"))
+                {
+                    Console.WriteLine("See you again! Byeeee");
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
