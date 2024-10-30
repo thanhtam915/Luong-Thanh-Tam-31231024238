b34964b2e36179c4208883de5ba42d0dabf89a44
thanhtam915 <tamluong.31231024238@st.ueh.edu.vn>
2024-10-31 06:49:58 +0700

    Session 5 update

--- Luong Thanh Tam 31231024238/Session5.cs
+++ Luong Thanh Tam 31231024238/Session5.cs
@@ -12,6 +12,7 @@ namespace Luong_Thanh_Tam_31231024238
     {
         public static void Main()
         {
+            //EX1
             //Console.WriteLine("Nhap 3 chu so cach nhau bang khoang trang: ");
             //string n1 = Console.ReadLine();
             //string[] n1string = n1.Split(' ');
@@ -19,8 +20,10 @@ namespace Luong_Thanh_Tam_31231024238
             //List<int> n1list = new List<int>(n1array);
             //int max = FindMax(n1list);
             //Console.WriteLine($"Gia tri max la {max}");
+            //----------------------------------------------------------------------------------------------
 
 
+            //EX2
             //int n;
             //do
             //{
@@ -37,9 +40,10 @@ namespace Luong_Thanh_Tam_31231024238
             //} while (true);
 
             //Console.WriteLine($"n = {n}, fractorial = {CaculateFactorial(n)}");
+            //----------------------------------------------------------------------------------------------
 
 
-
+            //EX3
             //Console.WriteLine("Nhap mot so duong: ");
             //int number;
             //bool res = int.TryParse(Console.ReadLine(), out number);
@@ -53,21 +57,55 @@ namespace Luong_Thanh_Tam_31231024238
             //{
             //    Console.WriteLine("Vui long nhap mot so nguyen duong.");
             //}
+            //-----------------------------------------------------------------------------------------------
 
 
-            Console.WriteLine("Nhap mot so duong: ");
-            int number;
-            bool res = int.TryParse(Console.ReadLine(), out number);
+            //EX4
+            //int n4a;
+            //do
+            //{
+            //    Console.WriteLine("Nhap mot so duong N (n4a):");
+            //} while (!int.TryParse(Console.ReadLine(), out n4a) || n4a <= 0);
 
-            if (res && number > 0)
-            {
-                Console.WriteLine($" {number} so nguyen to dau tien la:");
-                PrintNFirstPrimeNumbers(number);
-            }
-            else
-            {
-                Console.WriteLine("Vui long nhap mot so nguyen duong.");
-            }
+            //Console.WriteLine($"Cac so nguyen to < {n4a}:");
+            //PrintPrimeNumberLessThan(n4a); // In ra các số nguyên tố nhỏ hơn n4a
+
+            //// In N số nguyên tố đầu tiên
+            //int n4b;
+            //do
+            //{
+            //    Console.WriteLine("In N so nguyen to dau tien");
+            //    Console.WriteLine("Nhap mot so duong N (n4b):");
+            //} while (!int.TryParse(Console.ReadLine(), out n4b) || n4b <= 0);
+
+            //Console.WriteLine($"Cac so nguyen to dau tien ({n4b} so):");
+            //PrintNFirstPrimeNumbers(n4b);
+            //----------------------------------------------------------------------------------------------
+
+            //EX5
+            //int n5;
+            //Console.WriteLine("Kiem tra so hoan hao");
+            //do
+            //{
+            //    Console.WriteLine("Nhap gia tri n5 nguyen duong:");
+            //    bool res = int.TryParse(Console.ReadLine(), out n5);
+            //    if (!res)
+            //        Console.WriteLine("Nhap mot so nguyen, khong phai chuoi.");
+            //    else if (n5 < 0)
+            //        Console.WriteLine("Nhap mot so nguyen duong.");
+            //    else
+            //    {
+            //        Ex5(n5);
+            //        break;
+            //    }
+            //} while (true);
+            //----------------------------------------------------------------------------------------------
+
+
+            Console.WriteLine("Enter a string");
+            string string6 = Console.ReadLine();
+            Ex6(string6);
+
 
 
             Console.ReadKey();
@@ -122,24 +160,89 @@ namespace Luong_Thanh_Tam_31231024238
         }
         public static void PrintNFirstPrimeNumbers(int n4b)
         {
-            int count = 0;
-            int num = 2;
+            int count = 0; // Biến đếm số lượng số nguyên tố đã in
+            int num = 2;   // Số bắt đầu để kiểm tra tính nguyên tố
 
+            // Vòng lặp tiếp tục cho đến khi đã in ra đủ số nguyên tố
             while (count < n4b)
             {
+                // Kiểm tra xem số hiện tại có phải là số nguyên tố hay không
                 if (IsPrime(num))
                 {
-                    Console.Write(num + " ");
-                    count++;
+                    Console.Write(num + " "); // In ra số nguyên tố
+                    count++;                   // Tăng biến đếm lên 1
                 }
-                num++;
+                num++; // Tăng số hiện tại để kiểm tra số tiếp theo
             }
             Console.WriteLine();
         }
         public static void Ex5(int number5)
-        public static void Ex6(int number6)
         {
+            int sum = 0;
 
+            for (int i = 1; i <= Math.Sqrt(number5); i++)
+            {
+                if (number5 % i == 0)
+                {
+                    if (i == 1)
+                        sum += i;
+                    else
+                    {
+                        sum += i;
+                        if (i != number5 / i)
+                            sum += number5 / i;
+                    }
+                }
+            }
+            if (sum == number5)
+                Console.WriteLine($"{number5} is a perfect number!");
+            else
+                Console.WriteLine($"{number5} is not a perfect number!");
+            Console.WriteLine("Danh sach cac so hoan hao < 1000:");
+            for (int i = 1; i < 1000; i++)
+            {
+                int sum2 = 0;
+                for (int j = 1; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        if (j == 1)
+                            sum2 += j;
+                        else
+                        {
+                            sum2 += j;
+                            if (j != i / j)
+                                sum2 += i / j;
+                        }
+                    }
+                }
+                if (sum2 == i)
+                    Console.WriteLine($"{i}");
+            }
+        }
+        public static void Ex6(string input)
+        {
+            input = input.ToLower();
+
+            bool[] alphabetTracker = new bool[26];
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (c >= 'a' && c <= 'z')
+                {
+                    alphabetTracker[c - 'a'] = true;
+                }
+            }
+            for (int i = 0; i < alphabetTracker.Length; i++)
+            {
+                if (!alphabetTracker[i])
+                {
+                    Console.WriteLine("This string is not a pangram.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("This string is a pangram.");
         }
     }
 }
